036de2d13a532e21bebc39d27467cf86ed528e8b
Dan Mihaila <danmihaila@gmail.com>
2017-12-21 12:52:29 +0200

    test by dan

--- src/impactstory.php
+++ src/impactstory.php
@@ -215,7 +215,7 @@ get_header();
 
 													<?php foreach( $terms as $term ): ?>
 
-														<li><a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></li>
+														<li><a href="<?php echo get_term_link( $term ); ?>">--<?php echo $term ?>--<?php echo $term->name; ?>--</a></li>
 
 													<?php endforeach; ?>
 
