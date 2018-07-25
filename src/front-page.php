f8f00d2193c850a09e5eaf3f307353252c9fe64e
baripembo <erikawei@gmail.com>
2018-07-24 21:54:47 -0400

    adding impact story id to latest stories module

--- src/front-page.php
+++ src/front-page.php
@@ -18,12 +18,12 @@ get_header();
 **/
 
 $latest_stories_args = array (
-	'cat' => array(3, 105, 98, 110), //blog, dataviz, video, slideshow
+	'cat' => array(3, 105, 98, 110, 145), //blog, dataviz, video, slideshow, impact story
 	'posts_per_page' => 3, 
 	'orderby' => 'date'  
 );
 
-// set the number of posts per page
+// // set the number of posts per page
 // $posts_per_page = 3;
 
 // // get sticky posts array
@@ -100,7 +100,7 @@ if ($latest_posts->have_posts()) :
 /* LATEST PRESS
 **/
 $press_args = array (
-	'cat' => array(2,13,101,106), //article, case study, media 
+	'cat' => array(2,101), //article, case study
 	'posts_per_page' => 3
 );
 
