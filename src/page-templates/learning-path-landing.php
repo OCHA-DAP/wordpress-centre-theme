8aa448da08979f253d3f6016e0745eac978732cc
baripembo <erikawei@gmail.com>
2022-01-18 15:50:11 -0500

    learning path improvements wip

--- src/page-templates/learning-path-landing.php
+++ src/page-templates/learning-path-landing.php
@@ -13,7 +13,7 @@ get_header();
 <script>
 	//mixpanel tracking
 	window.onload = function(e) {
-		mpTrack.pageView(document.title, 'learning module');
+		//mpTrack.pageView(document.title, 'learning module');
 	}
 </script>
 
@@ -82,43 +82,23 @@ get_header();
 	$featured_image = get_post_thumbnail_id($post->ID);
 	if ($featured_image === '') $featured_image = $media;
 
-	/** Collect header data **/
-	if (isset($metabox_data['_uncode_header_type'][0]) && $metabox_data['_uncode_header_type'][0] !== '') {
-		// $page_header_type = $metabox_data['_uncode_header_type'][0];
-		// if ($page_header_type !== 'none') {
-		// 	$meta_data = uncode_get_specific_header_data($metabox_data, $post_type, $featured_image);
-		// 	$metabox_data = $meta_data['meta'];
-		// 	$show_title = $meta_data['show_title'];
-		// }
-	} else {
-		$page_header_type = ot_get_option('_uncode_'.$post_type.'_header');
-		if ($page_header_type !== '' && $page_header_type !== 'none') {
-			$metabox_data['_uncode_header_type'] = array($page_header_type);
-			$meta_data = uncode_get_general_header_data($metabox_data, $post_type, $featured_image);
-			$metabox_data = $meta_data['meta'];
-			$show_title = $meta_data['show_title'];
-		}
-	}
 
-	while ( have_posts() ) : the_post();
+	while ( have_posts() ) : the_post(); ?>
 
-		/** Build header **/
-		if ($page_header_type !== '' && $page_header_type !== 'none') {
-			$page_header = new unheader($metabox_data, $post->post_title);
+	<div id="page-header" data-imgready="true">
+		<div class="header-basic">
+			<div class="background-element header-wrapper header-scroll-opacity header-parallax style-color-rgdb-bg header-only-text" data-height="35">
+			<div id="particles-js"><canvas width="1572" height="600" style="width: 100%; height: 100%;"></canvas></div>
+			<div class="header-main-container limit-width">
+				<div class="header-content header-center header-middle header-align-center">
+					<div class="header-content-inner">
+						<h1 class="header-title"><span>Learn With The Centre</span></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-			$header_html = $page_header->html;
-			if ($header_html !== '') {
-				echo '<div id="page-header">';
-				echo uncode_remove_wpautop( $page_header->html );
-				echo '</div>';
-			}
-
-			if (!empty($page_header->poster_id) && $page_header->poster_id !== false && $media !== '') {
-				$media = $page_header->poster_id;
-			}
-		}
-		echo '<script type="text/javascript">UNCODE.initHeader();</script>';
-	?>
 
 	<article class="learning-path landing">
 		<div class="content-width">
@@ -133,7 +113,9 @@ get_header();
 					'post_type' => 'page',
 				    'meta_key' => '_wp_page_template',
 				    'hierarchical' => 0,
-				    'meta_value' => 'page-templates/learning-path-overview.php'
+				    'meta_value' => 'page-templates/learning-path.php',
+				    'sort_column' => 'post_date', 
+                    'sort_order' => 'DESC'
 				));
 				$section_pages = $pages;
 				$jump_menu_pages = $pages; 
@@ -141,15 +123,26 @@ get_header();
 
 			<div class="column-container">
 				<div class="column column-3">
+					<ol class="breadcrumb header-subtitle" vocab="http://schema.org/" typeof="BreadcrumbList">
+						<li property="itemListElement" typeof="ListItem">
+							<a href="https://centre.humdata.org/" itemprop="url">Home</a>
+						</li>
+						<li property="itemListElement" typeof="ListItem">
+							<a href="https://centre.humdata.org/" itemprop="url">Resources</a>
+						</li>
+						</li>
+						<li property="itemListElement" typeof="ListItem" class="current">All Learning Paths</li>
+					</ol>
 					<ul class="jump-menu">
-						<li>LEARNING PATHS</li>
+						<li>ALL LEARNING PATHS</li>
 						<?php
 							foreach($jump_menu_pages as $page) {
 								$page_slug = $page->post_name;
+								$page_order = $page->menu_order;
 								$page_query = new WP_Query('page_id=' . $page->ID);
-								while ($page_query->have_posts()) : $page_query->the_post(); 
-									if (get_field('excerpt')): ?>
-										<li><a href="#<?php echo $page_slug ?>"><?php echo the_title(); ?></a></li>
+								while ($page_query->have_posts()) : $page_query->the_post();
+									if ($page_order==0): ?>
+										<li><a href="<?php echo get_permalink($page->ID); ?>"><?php echo the_title(); ?></a></li>
 								<?php endif;
 								endwhile; 
 							}
@@ -157,36 +150,27 @@ get_header();
 					</ul>
 				</div>
 				<div class="column column-9">
-
+					<div class="column-container column-flow">
 				<?php
 					foreach($section_pages as $page){
 						$page_slug = $page->post_name;
+						$page_order = $page->menu_order;
 						$page_query = new WP_Query('page_id=' . $page->ID);
-						while ($page_query->have_posts()) : $page_query->the_post(); 
-							if (get_field('excerpt')): ?>
-							<section id="<?php echo $page_slug; ?>">
-								<h2 class="section-header"><?php echo the_title(); ?></h2>
-								<p><?php echo get_field('excerpt'); ?></p>
-								<?php $video = get_field('overview_video');
-									$videoID = $video['id'];
-									if ($videoID): ?>
-										<div class="feature-media">
-								      	<iframe id="overviewFeatureVideo" class="video-container" src="https://www.youtube.com/embed/<?php echo $video['id']; ?>
-								?modestbranding=1&rel=0&enablejsapi=1"></iframe>
-											<?php if ($video['title']): ?>
-												<div class="feature-media-caption">
-													<h3><?php echo $video['title']; ?></h3>
-													<p class="attribution"><?php echo $video['attribution']; ?></p>
-												</div>
-											<?php endif; ?>
-										</div>
-								<?php endif; ?>
-								<a class="button-primary" href="<?php echo get_page_link(); ?>">Learn How It Works</a>
-							</section>
+						while ($page_query->have_posts()) : $page_query->the_post();
+							if ($page_order==0): ?>
+								<div class="column column-4 landing-item">
+									<div class='thumb'>
+										<div class='category-label'><?php echo get_field('category_type'); ?></div>
+									</div>
+									<h6 class="subtitle"><?php echo get_field('category_type'); ?></h6>
+									<a href="<?php echo get_page_link(); ?>"><span class='title'><?php echo the_title(); ?></span></a>
+									<p><?php echo get_field('landing_excerpt'); ?></p>
+								</div>
 						<?php endif;
 						endwhile; 
 					} 
 				?>
+					</div>
 				</div>
 			</div>
 									
--- src/page-templates/learning-path.php
+++ src/page-templates/learning-path.php
@@ -8,14 +8,12 @@
  */
 
 get_header();
-
-$menu_name = get_field('menu_name');
 ?>
 
 <script>
 	//mixpanel tracking
 	window.onload = function(e) {
-		mixpanel.track('page view', { 'page type': 'learning module', 'learning module name': '<?php echo $menu_name ?>', 'learning module template': 'overview' });
+		//mixpanel.track('page view', { 'page type': 'learning module', 'learning module name': '<?php echo $menu_name ?>', 'learning module template': 'overview' });
 	}
 </script>
 
@@ -79,197 +77,177 @@ $menu_name = get_field('menu_name');
 		}
 	}
 
-	$media = get_post_meta($post->ID, '_uncode_featured_media', 1);
-	$media_display = get_post_meta($post->ID, '_uncode_featured_media_display', 1);
-	$featured_image = get_post_thumbnail_id($post->ID);
-	if ($featured_image === '') $featured_image = $media;
 
-	/** Collect header data **/
-	if (isset($metabox_data['_uncode_header_type'][0]) && $metabox_data['_uncode_header_type'][0] !== '') {
-		// $page_header_type = $metabox_data['_uncode_header_type'][0];
-		// if ($page_header_type !== 'none') {
-		// 	$meta_data = uncode_get_specific_header_data($metabox_data, $post_type, $featured_image);
-		// 	$metabox_data = $meta_data['meta'];
-		// 	$show_title = $meta_data['show_title'];
-		// }
-	} else {
-		$page_header_type = ot_get_option('_uncode_'.$post_type.'_header');
-		if ($page_header_type !== '' && $page_header_type !== 'none') {
-			$metabox_data['_uncode_header_type'] = array($page_header_type);
-			$meta_data = uncode_get_general_header_data($metabox_data, $post_type, $featured_image);
-			$metabox_data = $meta_data['meta'];
-			$show_title = $meta_data['show_title'];
-		}
-	}
+	$homeURL = esc_url( home_url( get_current_blog_id(), '/' ) );
 
-	while ( have_posts() ) : the_post();
-		/** Build header **/
-		if ($page_header_type !== '' && $page_header_type !== 'none') {
-			$page_header = new unheader($metabox_data, $post->post_title);
+	$curr_page = $post->menu_order;
+	$index_page_id = ($curr_page===0) ? $post->ID : $post->post_parent;
+	$category_type = ($curr_page===0) ? get_field('category_type') : get_field('category_type', $post->post_parent);
+	$index_page = get_page($index_page_id);
 
-			$header_html = $page_header->html;
-			if ($header_html !== '') {
-				echo '<div id="page-header">';
-				echo uncode_remove_wpautop( $page_header->html );
-				echo '</div>';
-			}
+	$pages = get_pages(array(
+	    'child_of' => $index_page_id
+	));
+	array_unshift($pages, $index_page); //add index page to pages array
+	$num_pages = count($pages);
+	$jump_menu_pages = $pages;
 
-			if (!empty($page_header->poster_id) && $page_header->poster_id !== false && $media !== '') {
-				$media = $page_header->poster_id;
-			}
-		}
-		echo '<script type="text/javascript">UNCODE.initHeader();</script>';
-	?>
+	while ( have_posts() ) : the_post(); ?>
 
-	<?php 
-		$menu_items = wp_get_nav_menu_items($menu_name);
-		include( locate_template( 'partials/menu-learningpath.php', false, false ) );
-	?>
-
-	<article class="learning-path">
-		<div class="feature-content">
-			<div class="content-width">
-				<div class="feature-inner">
-					<h1><?php echo get_the_title(); ?></h1>
-					<?php if (get_field('introduction')): ?>
-						<h3><?php echo get_field('introduction'); ?></h3>
-					<?php endif; ?>
+	<div id="page-header" data-imgready="true">
+		<div class="header-basic">
+			<div class="background-element header-wrapper header-scroll-opacity header-parallax style-color-rgdb-bg header-only-text" data-height="35">
+			<div id="particles-js"><canvas width="1572" height="600" style="width: 100%; height: 100%;"></canvas></div>
+			<div class="header-main-container limit-width">
+				<div class="header-content header-center header-middle header-align-center">
+					<div class="header-content-inner">
+						<h1 class="header-title"><span>Learn With The Centre</span></h1>
+					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<?php $video = get_field('overview_video');
-			$videoID = $video['id'];
-			if ($videoID): ?>
-				<div class="feature-media content-width">
-		      	<iframe id="overviewFeatureVideo" class="video-container" src="https://www.youtube.com/embed/<?php echo $video['id']; ?>
-		?modestbranding=1&rel=0&enablejsapi=1"></iframe>
-					<?php if ($video['title']): ?>
-						<div class="feature-media-caption">
-							<h3><?php echo $video['title']; ?></h3>
-							<p class="attribution"><?php echo $video['attribution']; ?></p>
-						</div>
-					<?php endif; ?>
-				</div>
-		<?php endif; ?>
+	<div class="content-width column-container">
+		<div class='column column-12'>
+			<ol class="breadcrumb header-subtitle" vocab="http://schema.org/" typeof="BreadcrumbList">
+				<li property="itemListElement" typeof="ListItem">
+					<a href="<?php echo $homeURL ?>" itemprop="url">Home</a>
+				</li>
+				<li property="itemListElement" typeof="ListItem">
+					<a href="<?php echo $homeURL ?>" itemprop="url">Resources</a>
+				</li>
+				</li>
+				<li property="itemListElement" typeof="ListItem">
+					<a href="<?php echo $homeURL ?>/learning-path" itemprop="url">All Learning Paths</a>
+				</li>
+				<li property="itemListElement" typeof="ListItem" class="current"><?php echo get_the_title(); ?></li>
+			</ol>	
+		</div>
+	</div>
 
-		<?php $threeColModule = get_field('3_column_module');
-			if ($threeColModule['column_1']['title'] && $threeColModule['column_2']['title'] && $threeColModule['column_3']['title']): ?>
-				<section class="section-3-col-module content-width">
-					<h2 class="section-header"><?php echo $threeColModule['title']; ?></h2>
-					<div class="column-container <?php if ($threeColModule['has_icons']) echo 'has-icons' ?>">
-						<?php for ($i=1; $i<4; $i++) { 
-							$column = $threeColModule['column_'.$i]; ?>
-							<div class="column column-4 background-gray">
-								<div class="column-inner">
-									<?php if ($threeColModule['has_icons']) { ?>
-										<img class="icon" src="<?php echo get_stylesheet_directory_uri() . '/assets/learning-path/icon-'. $column['icon_type'] .'.png' ?>" />
-									<?php } ?>
-									<h3 class="fixed-height"><?php echo $column['title']; ?></h3>
-									<p class="border-top"><?php echo $column['text']; ?></p>
-								</div>
-							</div>
-						<?php } ?>
-					</div>
-				</section>
-		<?php endif; ?>
+	<div class="content-width column-container">
+		<div class="column column-3">	
+			<ul class="jump-menu">
+				<li><h4><a href="<?php echo $homeURL ?>/learning-path">&#171; Back to all learning paths</a></h4></li>
+ 				<?php
+					foreach($jump_menu_pages as $key=>$page) {
+						$page_slug = $page->post_name;
+						$page_order = $page->menu_order; 
+						$class = ($key===($post->menu_order)) ? 'active' : ''; ?>
+							<li class="<?php echo ($key===0) ? 'primary' : 'secondary'; ?>"><a class="<?php echo $class; ?>" href="<?php echo get_permalink($page->ID); ?>"><?php echo $page->post_title; ?></a></li>
+						<?php
+					}
+				?>
+			</ul>
+		</div>
 
-		<?php $imageModule = get_field('module_with_image');
-			if ($imageModule['content']['title'] && $imageModule['content']['text']): ?>
-				<section class="section-module-with-img background-gray">
+		<div class="column column-9">
+
+			<article class="learning-path page-body">
+				<div class="feature-content">
 					<div class="content-width">
-						<h2 class="section-header"><?php echo $imageModule['title']; ?></h3>
-						<div class="column-container">
-							<div class="column column-4">
-								<h3><?php echo $imageModule['content']['title']; ?></h3>
-								<p><?php echo $imageModule['content']['text']; ?></p>
-							</div>
-							<div class="column column-8 align-right">
-								<?php $image = $imageModule['content']['image'];
-									if( !empty( $image ) ): ?>
-									    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-								<?php endif; ?>
-							</div>
+						<div class="feature-inner">
+							<div class='category-label'><?php echo $category_type; ?></div>
+							<h1><?php echo get_the_title(); ?></h1>
+							<?php if (get_field('introduction')): ?>
+								<?php echo get_field('introduction'); ?>
+							<?php endif; ?>
 						</div>
 					</div>
-				</section>
-		<?php endif; ?>
+				</div>
 
+				<?php $video = get_field('video');
+					$videoID = $video['id'];
+					if ($videoID): ?>
+						<div class="feature-media content-width">
+				      	<iframe id="overviewFeatureVideo" class="video-container" src="https://www.youtube.com/embed/<?php echo $video['id']; ?>
+				?modestbranding=1&rel=0&enablejsapi=1"></iframe>
+							<?php if ($video['title']): ?>
+								<div class="feature-media-caption">
+									<h3><?php echo $video['title']; ?></h3>
+									<p class="attribution"><?php echo $video['attribution']; ?></p>
+								</div>
+							<?php endif; ?>
+						</div>
+				<?php endif; ?>
 
-		<?php $faq = get_field('faq');
-			if ($faq['category_id']): ?>
-			<section class="section-faq content-width">
-				<div class="column-container">
-					<div class="column column-8">
+				<?php 
+					$column_module = get_field('column_module');
+					$columns = $column_module['columns'];
+
+					if ($column_module): ?>
+						<section class="column-module content-width">
+							<h2 class="section-header"><?php echo $column_module['title']; ?></h2>
+							<div class="column-container column-flow">
+								<?php foreach( $columns as $key=>$item ): ?>
+									<div class="column column-4">
+										<h3 class="fixed-height"><?php echo $item['title']; ?></h3>
+										<p class="border-top"><?php echo $item['description']; ?></p>
+									</div>
+								<?php endforeach; ?>
+							</div>
+						</section>
+				<?php endif; ?>
+
+				<?php $free_module = get_field('free_module');
+					if ($free_module): ?>
+						<section class="free-module content-width">
+							<h2 class="section-header"><?php echo $free_module['title']; ?></h2>
+							<?php echo $free_module['content']; ?>
+						</section>
+				<?php endif; ?>
+
+				<?php $faq = get_field('faq');
+					if ($faq['category_id']): ?>
+					<section class="section-faq content-width">
 						<h2 class="section-header"><?php echo $faq['title']; ?></h2>
 						<div class="accordion overview-accordion" id="faqAccordion">
 
-				<?php 
-				$id = $faq['category_id'];
-				$json = file_get_contents('https://centre.humdata.org/wp-json/wp/v2/ufaq?ufaq-category='.$id);
-				$items = json_decode($json);
-
-				foreach($items as $key=>$var) { ?>
-
-					<div class="card">
-						<div class="card-header" id="heading<?php echo $key ?>">
-							<h4 class="my-0">
-								<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse<?php echo $key ?>" aria-expanded="true" aria-controls="collapse<?php echo $key ?>">
-									<?php echo $items[$key]->title->rendered ?>
-								</button>
-							</h4>
-						</div>
-						<div id="collapse<?php echo $key ?>" class="collapse" aria-labelledby="heading<?php echo $key ?>" data-parent="#faqAccordion">
-							<div class="card-body">
-								<?php echo $items[$key]->content->rendered ?>
-							</div>
-						</div>
-					</div>
-				<?php } ?>
-							  
-					</div>
-				</div>
-			</section>
-		<?php endif; ?>
-
-		<?php 
-			$the_content = get_the_content();
-			$the_content = apply_filters('the_content', $the_content);
-			echo $the_content;
-		?>
-
-		<?php $cta = get_field('call_to_action'); 
-			if ($cta['title']):
-				$styleLight = strtolower($cta['style'])=='light' ? true : false;
-				$bg_color = ($styleLight) ? 'background-gray' : 'background-gray-dark';
-				$btn_style = ($styleLight) ? '' : 'button-dark'; ?>
-				<section class="section-call-to-action <?php echo $bg_color; ?>">
-					<div class="content-width align-center">
-						<h2 class="text-green"><?php echo $cta['title']; ?></h2>
-						<p><?php echo $cta['text']; ?></p>
 						<?php 
-							$link =  $cta['link'];
-							if ($link): 
-								$link_url = $link['url'];
-    							$link_title = $link['title'];
-    							$link_target = $link['target'] ? $link['target'] : '_self'; ?>
-								<a href="<?php echo esc_url($link_url); ?>" class="button-primary <?php echo $btn_style; ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-						<?php endif; ?>
-					</div>
-				</section>
-		<?php endif; ?>
+						$id = $faq['category_id'];
+						$json = file_get_contents('https://centre.humdata.org/wp-json/wp/v2/ufaq?ufaq-category='.$id);
+						$items = json_decode($json);
 
-		<section class="section-call-to-action background-gray-dark">
-			<div class="content-width align-center">
-				<h2 class="text-green">Learn more with us</h2>
-				<p>We offer assistance for anyone who wants to learn more</p>
-				<a href="#" class="request-support button-primary button-dark">Request Support</a>
-			</div>
-		</section>
+						foreach($items as $key=>$var) { ?>
 
-	</article>
-
-	<?php include( locate_template( 'partials/modal-learningpathsupport.php', false, false ) ); ?>
+							<div class="card">
+								<div class="card-header" id="heading<?php echo $key ?>">
+									<h4 class="my-0">
+										<button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse<?php echo $key ?>" aria-expanded="true" aria-controls="collapse<?php echo $key ?>">
+											<?php echo $items[$key]->title->rendered ?>
+										</button>
+									</h4>
+								</div>
+								<div id="collapse<?php echo $key ?>" class="collapse" aria-labelledby="heading<?php echo $key ?>" data-parent="#faqAccordion">
+									<div class="card-body">
+										<?php echo $items[$key]->content->rendered ?>
+									</div>
+								</div>
+							</div>
+						<?php } ?>
+									
+					</section>
+				<?php endif; ?>
+			</article>
+		</div>
+	</div>
+	<div class="content-width pagination-container">
+        <ul class="pagination">
+        	<li class="page-prev">
+        		<a class="btn btn-link <?php if ($curr_page===0) echo 'disabled'; ?>" href="<?php echo get_permalink($jump_menu_pages[$curr_page-1]->ID); ?>"><i class="fa fa-angle-left"></i> <span>Previous</span></a>
+        	</li>
+        	<?php
+				foreach($jump_menu_pages as $key=>$page) { ?>
+    				<li>
+		        		<span class="btn btn-link"><a class="page-numbers <?php echo ($curr_page===$key) ? 'current' : ''; ?>" href="<?php echo get_permalink($page->ID); ?>"><?php echo $key+1 ?></a></span>
+		        	</li>
+				<?php } ?>
+        	<li class="page-next">
+        		<a class="btn btn-link <?php if ($curr_page>=($num_pages-1)) echo 'disabled'; ?>" href="<?php echo get_permalink($jump_menu_pages[$curr_page+1]->ID); ?>"><span> Next</span> <i class="fa fa-angle-right"></i></a>
+        	</li>
+        </ul>
+    </div>
 	<?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
