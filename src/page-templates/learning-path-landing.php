eabf4c2811f71f4f5bce0dc5f91e0a51714687cf
baripembo <erikawei@gmail.com>
2022-02-10 12:06:13 -0500

    wip

--- src/page-templates/learning-path-landing.php
+++ src/page-templates/learning-path-landing.php
@@ -18,79 +18,15 @@ get_header();
 </script>
 
 <?php
-	/**
-	 * DATA COLLECTION - START
-	 **/
-
-	/** Init variables **/
-	$limit_width = $limit_content_width = $the_content = $main_content = $layout = $bg_color = $sidebar_style = $sidebar_bg_color = $sidebar = $sidebar_size = $sidebar_sticky = $sidebar_padding = $sidebar_inner_padding = $sidebar_content = $title_content = $media_content = $page_custom_width = $row_classes = $main_classes = $footer_content = $footer_classes = $content_after_body = '';
-	$with_builder = false;
-
-	$post_type = $post->post_type;
-
-	/** Get general datas **/
-	if (isset($metabox_data['_uncode_specific_style'][0]) && $metabox_data['_uncode_specific_style'][0] !== '') {
-		$style = $metabox_data['_uncode_specific_style'][0];
-		if (isset($metabox_data['_uncode_specific_bg_color'][0]) && $metabox_data['_uncode_specific_bg_color'][0] !== '') {
-			$bg_color = $metabox_data['_uncode_specific_bg_color'][0];
-		}
-	} else {
-		$style = ot_get_option('_uncode_general_style');
-		if (isset($metabox_data['_uncode_specific_bg_color'][0]) && $metabox_data['_uncode_specific_bg_color'][0] !== '') {
-			$bg_color = $metabox_data['_uncode_specific_bg_color'][0];
-		} else $bg_color = ot_get_option('_uncode_general_bg_color');
-	}
-	$bg_color = ($bg_color == '') ? ' style-'.$style.'-bg' : ' style-'.$bg_color.'-bg';
-
-	/** Get page width info **/
-	$boxed = ot_get_option('_uncode_boxed');
-
-	$page_content_full = (isset($metabox_data['_uncode_specific_layout_width'][0])) ? $metabox_data['_uncode_specific_layout_width'][0] : '';
-	if ($page_content_full === '') {
-		/** Use generic page width **/
-		$generic_content_full = ot_get_option('_uncode_'.$post_type.'_layout_width');
-		if ($generic_content_full === '') {
-			$main_content_full = ot_get_option('_uncode_body_full');
-			if ($main_content_full !== 'on') $limit_content_width = ' limit-width';
-		} else {
-			if ($generic_content_full === 'limit') {
-				$generic_custom_width = ot_get_option('_uncode_'.$post_type.'_layout_width_custom');
-				if ($generic_custom_width[1] === 'px') {
-					$generic_custom_width[0] = 12 * round(($generic_custom_width[0]) / 12);
-				}
-				if (is_array($generic_custom_width) && !empty($generic_custom_width)) {
-					$page_custom_width = ' style="max-width: '.implode('', $generic_custom_width).'; margin: auto;"';
-				}
-			}
-		}
-	} else {
-		/** Override page width **/
-		if ($page_content_full === 'limit') {
-			$limit_content_width = ' limit-width';
-			$page_custom_width = (isset($metabox_data['_uncode_specific_layout_width_custom'][0])) ? unserialize($metabox_data['_uncode_specific_layout_width_custom'][0]) : '';
-			if (is_array($page_custom_width) && !empty($page_custom_width) && $page_custom_width[0] !== '') {
-				if ($page_custom_width[1] === 'px') {
-					$page_custom_width[0] = 12 * round(($page_custom_width[0]) / 12);
-				}
-				$page_custom_width = ' style="max-width: '.implode("", $page_custom_width).'; margin: auto;"';
-			} else $page_custom_width = '';
-		}
-	}
-
-	$media = get_post_meta($post->ID, '_uncode_featured_media', 1);
-	$media_display = get_post_meta($post->ID, '_uncode_featured_media_display', 1);
-	$featured_image = get_post_thumbnail_id($post->ID);
-	if ($featured_image === '') $featured_image = $media;
-
 
 	while ( have_posts() ) : the_post(); ?>
 
-	<div id="page-header" data-imgready="true">
+	<div id="page-header">
 		<div class="header-basic">
-			<div class="background-element header-wrapper header-scroll-opacity header-parallax style-color-rgdb-bg header-only-text" data-height="35">
+			<div class="header-wrapper">
 			<div id="particles-js"><canvas width="1572" height="600" style="width: 100%; height: 100%;"></canvas></div>
 			<div class="header-main-container limit-width">
-				<div class="header-content header-center header-middle header-align-center">
+				<div class="header-content">
 					<div class="header-content-inner">
 						<h1 class="header-title"><span>Learn With The Centre</span></h1>
 					</div>
