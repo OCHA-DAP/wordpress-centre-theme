f022598a0d7262e4f4b5d053320a5fdc7a4e28d8
baripembo <erikawei@gmail.com>
2022-02-24 18:01:06 -0500

    removing old templates

--- src/scripts/learning-path.js
+++ src/scripts/learning-path.js
@@ -13,30 +13,7 @@
   				$(items[i]).removeClass('in');
   			}
   		}
-  	})
-
-	// modal events
-	$('.request-support').on('click', function(e) {
-		e.preventDefault();
-		$('.modal-support-form').show();
-	});
-	$('.close-btn').on('click', function(e) {
-		e.preventDefault();
-		$('.modal-support-form').hide();
-	});
-
-	//methodology read more content drawer events
-	$('.button-read-more').on('click', function(e) {
-		e.preventDefault();
-		$(this).toggleClass('expand');
-		$(this).closest('section').find('.content-drawer').slideToggle('fast');
-		if ($(this).hasClass('expand')) {
-			$(this).html('Close');
-		}
-		else {
-			$(this).html('Read More');
-		}
-	});
+  	});
 
 	// create yt players
 	readyYoutube();
