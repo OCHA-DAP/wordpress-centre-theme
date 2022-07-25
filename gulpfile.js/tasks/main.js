75ef30fb399c892faf46512c7fe72f5c0ec544b9
Catalin <>
2022-07-25 14:48:38 +0300

    gulp 3 to gulp 4

--- gulpfile.js/tasks/main.js
+++ gulpfile.js/tasks/main.js
@@ -3,11 +3,20 @@
 var gulp = require('gulp');
 
 // Default task chain: build -> (livereload or browsersync) -> watch
-gulp.task('default', ['watch']);
+gulp.task('default', function(done) {
+    gulp.series('watch')();
+    done();
+});
 
 // Build a working copy of the theme
-gulp.task('build', ['images', 'scripts', 'styles', 'theme']);
+gulp.task('build', function(done) {
+    gulp.series('images', 'scripts', 'styles', 'theme')();
+    done();
+});
 
 // Dist task chain: wipe -> build -> clean -> copy -> compress images
 // NOTE: this is a resource-intensive task!
-gulp.task('dist', ['images-optimize']);
+gulp.task('dist', function(done) {
+    gulp.series('images-optimize')();
+    done();
+});
