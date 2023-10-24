1e13e2117959f49530d9ae11b8649c0ecbaaf337
Catalin <catalinflorentinion@gmail.com>
2023-10-24 22:55:23 +0300

    HDX-9242 update Twitter logo to X

--- src/front-page.php
+++ src/front-page.php
@@ -82,7 +82,11 @@ if (is_front_page()) { ?>
 				<div class="col-lg-2"></div>
 				<div class="col-lg-7">
 					<div class="tweet">
-						<a href="http://www.twitter.com/humdata" target="_blank" class="twitter-logo"><div class="fa fa-twitter twhite" aria-hidden="true"></div></a>
+						<a href="http://www.x.com/humdata" target="_blank" class="twitter-logo">
+                            <span class="x-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1200 1227"><path fill="currentColor" d="M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"/></svg>
+                            </span>
+                        </a>
 						<div class="tweet-content">
 							<p class="tweet-text"><span></span></p>
 							<p class="author"></p>
